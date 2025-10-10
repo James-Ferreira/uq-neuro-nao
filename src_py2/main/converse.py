da79bdcb518dab9752dbac77ffb5dff5a1b18247
Conrad-Jacob-MacMini <vanman-lab@proton.me>
2025-10-10 14:07:24 +1000

    conversation manager file with Converse class created. converse.py updated accordingly

--- src_py2/main/converse.py
+++ src_py2/main/converse.py
@@ -2,25 +2,11 @@
 
 from __future__ import absolute_import
 
-from src_py2.robot.nao_robot import NAORobot
+from src_py2.robot.conversation_manager import Converse
 
+converser = Converse("meta")
 
-name = raw_input("Choose a robot: meta / clas: ")
-
-if name == "meta":
-    ips = ["192.168.0.78", "192.168.0.79"]
-elif name == "clas":
-    ips = ["192.168.0.183"]
-else:
-    ips = []
-
-connected = []
-for ip in ips:
-    try:
-        robot_converser = NAORobot(name, ip)
-        connected.append(robot_converser)
-    except Exception as e:
-        print("WARN: could not connect to {} ({}) - skipping".format(ip, e))
 
 if __name__ == "__main__":
-    robot_converser.tts.say("butternuts")
+    
+    converser.say("coconut sucker")
